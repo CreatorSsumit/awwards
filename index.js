04b7c84d60029f3d1ea8ba63fca6a83a3c3e4160
sumitkumar <pksumitkumar98@gmail.com>
2021-11-29 12:56:40 -0800

    DD

--- index.js
+++ index.js
@@ -2,37 +2,52 @@ const app = new PIXI.Application();
 document.querySelector("#scene").appendChild(app.view);
 
 // Inner radius of the circle
-const radius = 150;
+var radius;
 
 // The blur amount
 const blurSize = 28;
 
-app.loader.add('grass', './img/full_hero.a083b77.png');
-app.loader.load(setup);
+function anim(radius) {
+    app.loader.add('grass', './img/full_hero.a083b77.png');
+    app.loader.load(setup);
 
-function setup(loader, resources) {
-    const background = new PIXI.Sprite(resources.grass.texture);
-    app.stage.addChild(background);
-    background.width = app.screen.width;
-    background.height = app.screen.height;
+    function setup(loader, resources) {
+        const background = new PIXI.Sprite(resources.grass.texture);
+        app.stage.addChild(background);
+        background.width = app.screen.width;
+        background.height = app.screen.height;
 
-    const circle = new PIXI.Graphics()
-        .beginFill(0xFF0000)
-        .drawCircle(radius + blurSize, radius + blurSize, radius)
-        .endFill();
-    circle.filters = [new PIXI.filters.BlurFilter(blurSize)];
-    const bounds = new PIXI.Rectangle(0, 0, (radius + blurSize) * 2, (radius + blurSize) * 2);
-    const texture = app.renderer.generateTexture(circle, PIXI.SCALE_MODES.NEAREST, 1, bounds);
-    const focus = new PIXI.Sprite(texture);
-    app.stage.addChild(focus);
+        var circle = new PIXI.Graphics()
+            .beginFill(0xFF0000)
+            .drawCircle(radius + blurSize, radius + blurSize, radius)
+            .endFill();
+        circle.filters = [new PIXI.filters.BlurFilter(blurSize)];
+        var bounds = new PIXI.Rectangle(0, 0, (radius + blurSize) * 2, (radius + blurSize) * 2);
+        var texture = app.renderer.generateTexture(circle, PIXI.SCALE_MODES.NEAREST, 1, bounds);
+        var focus = new PIXI.Sprite(texture);
+        app.stage.addChild(focus);
 
-    background.mask = focus;
+        background.mask = focus;
 
-    app.stage.interactive = true;
-    app.stage.on('mousemove', pointerMove);
+        app.stage.interactive = true;
+        app.stage.on('mousemove', pointerMove);
 
-    function pointerMove(event) {
-        focus.position.x = event.data.global.x - focus.width / 2;
-        focus.position.y = event.data.global.y - focus.height / 2;
+
+        function pointerMove(event) {
+
+            document.querySelector('#circle').addEventListener('mouseenter', e => {
+                document.querySelector('canvas').width = '100%';
+            })
+            document.querySelector('#circle').addEventListener('mouseleave', e => {
+                document.querySelector('canvas').width = '800';
+
+
+            })
+
+
+            focus.position.x = event.data.global.x - focus.width / 2;
+            focus.position.y = event.data.global.y - focus.height / 2;
+        }
     }
 }
+anim(150)
