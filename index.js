4483ebf4c4ba91843162ec026903a92ecba2b568
CreatorSsumit <53761887+CreatorSsumit@users.noreply.github.com>
2021-12-02 11:03:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -51,3 +51,23 @@ function anim(radius) {
     }
 }
 anim(150)
+
+
+
+jQuery(document).ready(function() {
+
+  var mouseX = 0, mouseY = 0;
+  var xp = 0, yp = 0;
+   
+  $(document).mousemove(function(e){
+    mouseX = e.pageX - 30;
+    mouseY = e.pageY - 30; 
+  });
+    
+  setInterval(function(){
+    xp += ((mouseX - xp)/6);
+    yp += ((mouseY - yp)/6);
+    $("#circle").css({left: xp +'px', top: yp +'px'});
+  }, 20);
+
+});
